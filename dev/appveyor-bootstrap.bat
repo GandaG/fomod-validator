be89c793b6cfe514a4ae26a5d0d92ebf7c42ea29
Daniel Nunes <gandaganza@gmail.com>
2016-05-27 01:38:37 +0000

    Removed call command.

--- dev/appveyor-bootstrap.bat
+++ dev/appveyor-bootstrap.bat
@@ -6,11 +6,8 @@ conda create -y -n fomod-validator^
  -c https://conda.anaconda.org/mmcauliffe^
  -c https://conda.anaconda.org/anaconda^
  pyqt5=5.5.1 python=3.5.1 lxml=3.5.0
-call activate fomod-validator
-echo "Done activating."
-python -m pip install pip -U
-echo "Upgraded pip."
+activate fomod-validator
+
+pip install pip -U
 pip install setuptools -U --ignore-installed
-echo "Upgraded setuptools."
 pip install -r dev\reqs.txt
-echo "Installed reqs."
