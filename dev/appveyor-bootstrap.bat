77f69192013dce749af78dd95c80f49ed3bf5c99
Daniel Nunes <gandaganza@gmail.com>
2016-05-27 01:49:53 +0000

    Moved build command to batch file.

--- dev/appveyor-bootstrap.bat
+++ dev/appveyor-bootstrap.bat
@@ -6,8 +6,10 @@ conda create -y -n fomod-validator^
  -c https://conda.anaconda.org/mmcauliffe^
  -c https://conda.anaconda.org/anaconda^
  pyqt5=5.5.1 python=3.5.1 lxml=3.5.0
-activate fomod-validator
+call activate fomod-validator
 
 pip install pip -U
 pip install setuptools -U --ignore-installed
 pip install -r dev\reqs.txt
+
+inv build
